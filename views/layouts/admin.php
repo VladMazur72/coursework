1875802b7f5cc52309154a8c5397e997ac7f2121
VladMazur72 <vladmazurofficial@gmail.com>
2021-01-28 02:29:49 +0200

    Test2

--- views/layouts/admin.php
+++ views/layouts/admin.php
@@ -38,7 +38,7 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
+            ['label' => 'Home', 'url' => ['/admin/default/index']],
             ['label' => 'Articles', 'url' => ['/admin/article/index']],
             ['label' => 'Categories', 'url' => ['/admin/category/index']],
             ['label' => 'Tag', 'url' => ['/admin/tag/index']],
